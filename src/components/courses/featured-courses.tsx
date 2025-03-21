3a58cb80d09e0c2dc5d2d60e54b95f3e80faf425
Freshia-Njoki <fnjokimacharia@gmail.com>
2025-03-21 13:55:37 +0300

    line svg fixed - at categories and courses

--- src/components/courses/featured-courses.tsx
+++ src/components/courses/featured-courses.tsx
@@ -37,7 +37,26 @@ const FeaturedCoursesSection = () => {
   return (
     <section className="featured-courses py-5">
       <div className="container">
-        <h2 className="display-4 mb-4 text-center">Featured Courses</h2>
+      <h2 className="display-4 mb-4">
+  Featured <span style={{ color: "#008080" }}>Courses</span>
+  <br />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="151"
+    height="13"
+    viewBox="0 0 151 13"
+    fill="none"
+    style={{ textAlign: "right", marginLeft: "17rem", marginTop: "-5rem" }}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M144.802 10.9143C81.4865 -4.02447 26.0413 5.35568 6.60412 10.8145C4.92012 11.2874 2.52954 12.6543 1.26461 12.0247C-0.000322044 11.3951 0.339402 9.00736 2.0234 8.53442C23.4523 2.51625 82.1684 -7.15917 148.569 8.43463C150.401 8.82352 151.042 11.1878 150.002 11.8725C148.962 12.5572 146.634 11.3032 144.802 10.9143Z"
+      fill="#008080"
+    />
+  </svg>
+</h2>
+
         <div className="row">
           {featuredCourses.map((course) => (
             <div className="col-md-3 mb-4" key={course.id}>
