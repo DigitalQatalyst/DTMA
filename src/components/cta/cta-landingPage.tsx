3d98bebc223627e867f2f112bbe8deaa0a28b1ae
Freshia-Njoki <fnjokimacharia@gmail.com>
2025-03-26 23:16:39 +0300

    gradients - white    enlarged hero section image     green lines alignment     FC right and left same line FC cards - interactivity

--- src/components/cta/cta-landingPage.tsx
+++ src/components/cta/cta-landingPage.tsx
@@ -5,18 +5,15 @@ import Link from "next/link";
 const CTASection = () => {
   return (
     <section className="cta-section py-5 text-center text-white" 
-    style={{background:
-      "linear-gradient(186deg, #0C7D81 -3.07%, rgb(255, 255, 255) 95.2%)",
-  }}
     >
       <div className="container">
-        <h2 className="display-4 mb-2 text-light">Stay Ahead in Digital <span style={{ color: "#008080" }}>Transformation</span></h2>
+        <h2 className="display-4 mb-2">Stay Ahead in Digital <span style={{ color: "#008080" }}>Transformation</span></h2>
         <img
             src="/assets/img/icon/line-longer.svg"
             alt="line Icon"
             style={{ marginLeft: "30rem", marginTop: "-3rem" }}
           />
-        <p style={{color: "#DBDADE", marginBottom: "3rem"}}>
+        <p style={{color: "#636366", marginBottom: "3rem"}}>
         Get expert insights, AI trends & career-boosting tips – straight to your inbox!
         </p>
           <div className="subscription-form mt-4" style={{ width: "80%", margin: "0 auto" }}>
@@ -30,7 +27,7 @@ const CTASection = () => {
                 type="email"
                 // className="form-control"
                 placeholder="Enter your email address"
-                style={{ borderRadius: '50px', color: "#636366", width: "40rem", paddingLeft: "3.5rem",  border: '2px solid #DBDADE', background: "rgb(201, 224, 224)"}}
+                style={{ borderRadius: '50px', color: "#636366", width: "40rem", paddingLeft: "3.5rem",  border: '2px solid #DBDADE'}}
               />
               <button className="btn mt-2 mb-2 text-white" type="button" id="button-addon2" style={{ borderRadius: '50px', backgroundColor:"#008080", marginLeft: "-9rem", padding: "5px", fontSize: "16px"}}>
                 Get Insights Now
--- src/components/hero-area/hero-section-landingPage.tsx
+++ src/components/hero-area/hero-section-landingPage.tsx
@@ -3,34 +3,27 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="hero-section position-relative overflow-hidden">
+    <section className="hero-section position-relative" style={{marginBottom: "7rem"}}>
       {/* Hero Background */}
       <div className="hero-bg position-absolute w-100 h-100">
         <img
           src="/assets/img/Hero image.jpg"
           alt="Hero Background"
-          className="w-100 h-100 object-fit-cover"
+          className="w-100 h-200 object-fit-cover"
           style={{ objectPosition: "top center" }}
         />
       </div>
 
       {/* Hero Content */}
-      <div className="container position-relative z-index-2 text-center text-white py-5">
-        {/* Embrace Change Title */}
+      <div className="container position-relative z-index-2 text-center text-white" style={{paddingTop:"10rem"}}>
+  
         <h1 className="display-4 mb-3" style={{ color: "#ffffff" }}>
           <strong>Embrace Change</strong>
         </h1>
 
-        {/* Additional Content */}
         <p className="mb-4" style={{ fontSize: "1.2rem", color: "rgba(255, 255, 255, 0.80)"}}>
           Join our expert-led courses on <strong style={{ color: "#008080" }}>Digital Transformation Management Academy</strong> <br></br>to propel your career in the digital transformation journey.
         </p>
-
-        {/* Get Started Button */}
-        {/* <Link href="/courses" className="btn btn-lg btn-primary mb-3" style={{ backgroundColor: "#00A9A5", color: "#fff" }}>
-          Get Started
-        </Link> */}
-
 
         <div className="mb-3">
           <input
@@ -40,8 +33,8 @@ const HeroSection = () => {
             style={{ width: "300px", display: "inline-block", borderRadius: "0px 0px 0px 0px" }}
           />
           <button
-            className="btn ms-0"
-            style={{ height: "52px", borderRadius: "0px 10px 10px 0px", backgroundColor: "#008080", color: "#fff" }}
+            className="btn"
+            style={{ height: "55px", borderRadius: "0px 10px 10px 0px", backgroundColor: "#008080", color: "#fff", marginBottom: "3px" }}
           >
             <img src="assets/img/icon/frame.svg" alt="Robot Icon" className="ms-2" style={{ width: "20px", height: "20px" }} />
             Ask DTMA AI
@@ -49,14 +42,14 @@ const HeroSection = () => {
         </div>
 
         <button
-          className="btn position-absolute bottom-0 end-0"
+          className="btn position-absolute"
           style={{
             backgroundColor: "#00A9A5",
             color: "#fff",
             borderRadius: "10px",
             right: "0px",
-            // transform: "translateY(70%)",
-            // zIndex: 9999
+            marginTop: "6.5rem",
+            marginRight: "-5.7rem"
           }}
         >
           <img src="assets/img/icon/frame.svg" alt="Robot Icon" className="ms-2" style={{ width: "20px", height: "20px" }} />
