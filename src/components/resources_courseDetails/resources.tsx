b11b8ff8bc6c5898db165fb2e3ae10500c0b7e7c
Freshia-Njoki <fnjokimacharia@gmail.com>
2025-04-04 01:26:12 +0300

    Resources section complete

--- src/components/resources_courseDetails/resources.tsx
+++ src/components/resources_courseDetails/resources.tsx
@@ -1,17 +1,38 @@
-
+import React from 'react';
 
 function Resources() {
   const resourcesData = [
     { fileName: "The digital tools expert's manual", sizeType: "PDF | 2.5 MB", lastModified: "Yesterday" },
     { fileName: "The digital tools expert's manual", sizeType: "PDF | 2.5 MB", lastModified: "2 days ago" },
     { fileName: "The digital tools expert's manual", sizeType: "PDF | 2.5 MB", lastModified: "1 week ago" },
-    // Add more resource items
   ];
 
   return (
-    <div>
-      {/* Other page content */}
-      <Resources resource={resourcesData} />
+    <div className="resources-container">
+      <div className="resources-button-container">
+        <button className="resources-button active">Downloads</button>
+        <button className="resources-button">External Links</button>
+        <button className="resources-button">Templates & Worksheets</button>
+      </div>
+
+      <table className="resources-table">
+        <thead>
+          <tr>
+            <th className="resources-table-header">File Name</th>
+            <th className="resources-table-header">Size & Type</th>
+            <th className="resources-table-header">Last Modified</th>
+          </tr>
+        </thead>
+        <tbody>
+          {resourcesData.map((item, index) => (
+            <tr className="resources-table-row" key={index}>
+              <td className="resources-table-data">{item.fileName}</td>
+              <td className="resources-table-data">{item.sizeType}</td>
+              <td className="resources-table-data">{item.lastModified}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
