91acf5961ce87432a7631f06309fa6be7f2bc10a
DigitalQatalyst <josesyanwa@gmail.com>
2025-06-16 20:26:20 +0300

    create navbar

--- src/app/(courses)/layout.tsx
+++ src/app/(courses)/layout.tsx
@@ -1,8 +1,8 @@
 import BackToTop from "@/components/back-to-top";
 import Footer from "@/components/footer/dtma";
 import PreFooter from "@/components/footer/footer-landingPage";
-// import HeaderTwo from "@/components/header/header-two";
-import HeaderTwo from "@/components/header/navbar/dtma-navbar";
+import HeaderTwo from "@/components/header/header-two";
+// import HeaderTwo from "@/components/header/navbar/dtma-navbar";
 import MainProvider from "@/components/provider/main-provider";
 
 export default function Layout({children}: {children:React.ReactNode}) {
