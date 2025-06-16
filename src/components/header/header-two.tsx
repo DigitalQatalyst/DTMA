7190b54c5972d0c3c5a1db75ad95be37b6e70631
DigitalQatalyst <josesyanwa@gmail.com>
2025-06-16 20:26:20 +0300

    create navbar

--- src/components/header/header-two.tsx
+++ src/components/header/header-two.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import SearchButton from "./button/search-button";
-import NavbarMenusTwo from "./navbar/navbar-menus-2";
-import logo from "@/assets/img/logo/logo-black.png";
-import ProfileDropdown from "./header-profile/profile-dropdown";
 import HeaderCategoryArea from "./header-category/header-category-area";
 import HeaderStickyWrapper from "./header-sticky-provider/header-sticky-wrapper";
 import CartButton from "./button/cart-button";
 import OffcanvasButton from "./button/offcanvas-btn";
+import SignInButton from "./sign-in-button/SignInButton"; // Import the separate component
 
 // prop type 
 type IProps = {
@@ -27,36 +25,28 @@ export default function HeaderTwo({ inner = false, transparent }: IProps) {
                 <div className="tp-header-2-right d-flex align-items-center">
                   <div className="tp-header-inner-logo tp-header-logo">
                     <Link href="/">
-                      <Image src={logo} alt="logo" priority style={{ height: 'auto' }} />
+                      <Image src="/assets/dtma/dtma_logo.svg" alt="logo" width={100} height={50} priority style={{ height: 'auto' }} />
                     </Link>
                   </div>
-                  <HeaderCategoryArea />
                 </div>
               </div>
               <div className="col-xxl-6 col-xl-7 col-lg-6 d-none d-xl-block">
                 <div className="main-menu text-xl-center d-none d-xl-block">
-                  <NavbarMenusTwo />
+                  <nav>
+                    <ul>
+                      <li><a href="#">Home</a></li>
+                      <li><a href="#">Courses</a></li>
+                      <li><a href="#">About</a></li>
+                      <li><a href="#">Help & Support</a></li>
+                      <li style={{ marginTop: '5px' }}><SearchButton /></li>
+                    </ul>
+                  </nav>
                 </div>
               </div>
               <div className="col-xxl-3 col-xl-2 col-lg-6 col-6">
                 <div className="tp-header-2-contact d-flex align-items-center justify-content-end">
-                  <div className="tp-header-inner-search">
-                    <SearchButton/>
-                  </div>
-                  <div className="tp-header-2-cart home-2 d-none d-xxl-block">
-                    <CartButton/>
-                  </div>
-                  <div className={`tp-header-inner-btn ${inner ? '' : 'home-2'} d-none d-xxl-block`}>
-                    <Link className="tp-btn-inner" href="/course-with-filter">
-                      Enroll Now
-                    </Link>
-                  </div>
-                  {/* profile dropdown */}
-                  <ProfileDropdown/>
-                  {/* profile dropdown */}
-                  <div className="offcanvas-btn d-xxl-none ml-30">
-                    <OffcanvasButton offcanvas_cls="offcanvas__2" offcanvas_menu_2={true}/>
-                  </div>
+                  <Image src="/assets/dtma/lock.svg" alt="lock" width={16} height={16} style={{ marginRight: '10px' }} />
+                  <SignInButton />
                 </div>
               </div>
             </div>
---
+++ src/components/header/sign-in-button/SignInButton.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const SignInButton = () => (
+  <div 
+    style={{
+      display: 'flex',
+      height: '40px',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: '8px',
+      borderRadius: '8px',
+      border: '1px solid #008080',
+      color: '#008080',
+      textAlign: 'center',
+      fontFamily: 'Outfit',
+      fontSize: '14px',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      lineHeight: '20px',
+      letterSpacing: '0.1px',
+      padding: '0 20px'
+    }}
+  >
+    Sign In
+  </div>
+);
+
+export default SignInButton;
