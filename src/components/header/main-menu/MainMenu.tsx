a54bccbe7c7a7603a6476655979a05fb57048855
Digitalqatalyst <dq.demo.a@digitalqatalyst.com>
2025-06-20 17:01:30 +0300

    navlinks added

--- src/components/header/main-menu/MainMenu.tsx
+++ src/components/header/main-menu/MainMenu.tsx
@@ -4,24 +4,42 @@ import SearchButton from "../button/search-button";
 
 export default function MainMenu() {
   const linkStyles = {
-    color: 'rgba(47, 43, 61, 0.90)',
+    color: "rgba(47, 43, 61, 0.90)",
     fontFeatureSettings: "'liga' off, 'clig' off",
-    fontFamily: 'Outfit',
-    fontSize: '15px',
-    fontStyle: 'normal',
+    fontFamily: "Outfit",
+    fontSize: "15px",
+    fontStyle: "normal",
     fontWeight: 500,
-    lineHeight: '22px', /* 146.667% */
+    lineHeight: "22px" /* 146.667% */,
   };
 
   return (
     <div className="main-menu text-xl-center d-none d-xl-block">
       <nav>
         <ul>
-          <li><Link href="/" style={linkStyles}>Home</Link></li>
-          <li><Link href="/course-with-filter" style={linkStyles}>Courses</Link></li>
-          <li><Link href="#" style={linkStyles}>About</Link></li>
-          <li><Link href="#" style={linkStyles}>Help & Support</Link></li>
-          <li style={{ marginTop: '35px' }}><SearchButton /></li>
+          <li>
+            <Link href="/" style={linkStyles}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/course-with-filter" style={linkStyles}>
+              Courses
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" style={linkStyles}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" style={linkStyles}>
+              Help & Support
+            </Link>
+          </li>
+          <li style={{ marginTop: "35px" }}>
+            <SearchButton />
+          </li>
         </ul>
       </nav>
     </div>
