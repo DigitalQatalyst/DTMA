7a1f05de12eaa943ba85ff47bca10efbec6ecb11
Digitalqatalyst <dq.demo.a@digitalqatalyst.com>
2025-06-25 07:06:10 -0700

    with header nine

--- src/app/(courses)/layout.tsx
+++ src/app/(courses)/layout.tsx
@@ -1,6 +1,7 @@
 import BackToTop from "@/components/back-to-top";
 import FooterTwo from "@/components/footer/footer-two";
 import CustomHeader from "@/components/header/custom-header/CustomHeader";
+import HeaderNine from "@/components/header/header-nine";
 // import Footer from "@/components/footer/dtma";
 // import PreFooter from "@/components/footer/footer-landingPage";
 import HeaderTwo from "@/components/header/header-two";
@@ -13,21 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       {/* header area start */}
       {/* <HeaderTwo /> */}
       {/* header area end */}
-      <CustomHeader
-        className="header-area p-relative"
-        style={{
-          background:
-            "linear-gradient(186deg, rgba(12, 125, 129, 0.50) -3.07%, rgba(12, 125, 129, 0.00) 95.2%)",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
-          minHeight: "200px", // Adjust as needed
-        }}
-      >
-        <HeaderTwo />
-      </CustomHeader>
+      <HeaderNine />
 
       {/* main content */}
       {children}
